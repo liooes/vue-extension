c6963d89636d8198ea53e6fc5d42ad104165d3e2
liooes <282018284@qq.com>
2023-03-09 21:20:09 +0800

    实现了界面的主体

--- src/view/em.vue
+++ src/view/em.vue
@@ -1,7 +1,152 @@
 <template>
-  <el-button type="primary">hhh</el-button>
-</template> 
- 
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="400px">
+          <el-row class="tac">
+            <el-col :span="12">
+              <el-menu active-text-color="#ffd04b" background-color="#545c64" class="el-menu-vertical-demo"
+                default-active="2" text-color="#fff" @open="handleOpen" @close="handleClose">
+                <el-sub-menu index="1">
+                  <template #title>
+                    <el-icon>
+                      <location />
+                    </el-icon>
+                    <span>Navigator One</span>
+                  </template>
+                  <el-menu-item-group title="Group One">
+                    <el-menu-item index="1-1">item one</el-menu-item>
+                    <el-menu-item index="1-2">item two</el-menu-item>
+                  </el-menu-item-group>
+                  <el-menu-item-group title="Group Two">
+                    <el-menu-item index="1-3">item three</el-menu-item>
+                  </el-menu-item-group>
+                  <el-sub-menu index="1-4">
+                    <template #title>item four</template>
+                    <el-menu-item index="1-4-1">item one</el-menu-item>
+                  </el-sub-menu>
+                </el-sub-menu>
+                <el-menu-item index="2">
+                  <el-icon><icon-menu /></el-icon>
+                  <span>Navigator Two</span>
+                </el-menu-item>
+                <el-menu-item index="3" disabled>
+                  <el-icon>
+                    <document />
+                  </el-icon>
+                  <span>Navigator Three</span>
+                </el-menu-item>
+                <el-menu-item index="4">
+                  <el-icon>
+                    <setting />
+                  </el-icon>
+                  <span>Navigator Four</span>
+                </el-menu-item>
+              </el-menu>
+        </el-aside>
+        <el-main>
+          <el-row>
+            <el-col :span="4">
+              <el-form-item label="项目：">
+                <el-select v-model="value" class="m-2" placeholder="Select" size="large">
+                  <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="单号：">
+                <el-input v-model="value" placeholder="请输入单号"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="创建时间：">
+                <el-date-picker v-model="pickervalue" type="daterange" unlink-panels range-separator="To"
+                  start-placeholder="Start date" end-placeholder="End date" :shortcuts="shortcuts" :size="size" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="4">
+              <el-form-item label="提交人：">
+                <el-select v-model="value" class="m-2" placeholder="Select" size="large">
+                  <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="4">
+              <el-form-item label="完结人：">
+                <el-select v-model="value" class="m-2" placeholder="Select" size="large">
+                  <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="4">
+              <el-form-item label="交互对象：">
+                <el-select v-model="value" class="m-2" placeholder="Select" size="large">
+                  <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="完结时间：">
+                <el-date-picker v-model="pickervalue" type="daterange" unlink-panels range-separator="To"
+                  start-placeholder="Start date" end-placeholder="End date" :shortcuts="shortcuts" :size="size" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="4">
+              <el-form-item label="问题类别：">
+                <el-select v-model="value" class="m-2" placeholder="Select" size="large">
+                  <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="4">
+              <el-form-item label="问题类型：">
+                <el-select v-model="value" class="m-2" placeholder="Select" size="large">
+                  <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="4">
+              <el-form-item label="承运商：">
+                <el-select v-model="value" class="m-2" placeholder="Select" size="large">
+                  <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="4">
+              <el-form-item label="回复状态：">
+                <el-select v-model="value" class="m-2" placeholder="Select" size="large">
+                  <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+                </el-select>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="4">
+              <el-form-item label="机器人服务状态：">
+                <el-select v-model="value" class="m-2" placeholder="Select" size="large">
+                  <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="4">
+              <el-form-item label="是否多包裹：">
+                <el-select v-model="value" class="m-2" placeholder="Select" size="large">
+                  <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+                </el-select>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
   <style scoped>
   .layout-container-demo .el-header {
     position: relative;
