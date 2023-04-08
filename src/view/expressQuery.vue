32cccc4cd996b0fa174610fa93f36924e358c036
liooes <282018284@qq.com>
2023-04-09 00:54:40 +0800

    1.界面新增表格

--- src/view/expressQuery.vue
+++ src/view/expressQuery.vue
@@ -10,17 +10,24 @@
         </el-input>
     </div>
     <el-progress :text-inside="true" :stroke-width="20" :percentage="percent" />
+    <el-table :data="tableData" stripe border>
+    <el-table-column sortable prop="expressnumber" label="expressnumber"></el-table-column>
+    <el-table-column sortable prop="latest_time_new" label="latest_time_new"></el-table-column>
+    <el-table-column sortable prop="latest_progress" label="latest_progress"></el-table-column>
+    <el-table-column sortable prop="current" label="current"></el-table-column>
+    <!-- <el-table-column prop="context" label="context"></el-table-column> -->
+    <el-table-column prop="com" label="com"></el-table-column>
+  </el-table>
 </template>
 
 <script>
 import { ref } from 'vue';
 import axios from 'axios';
-import { ElProgress,ElNotification } from 'element-plus'
-
+import { ElProgress, ElNotification } from 'element-plus'
 import * as XLSX from 'xlsx'
 
-
 const inputnumber = ref('');
+var tableData = [];
 var tokenV2 = '';
 const gettokenV2API = 'https://www.baidu.com/s?tn=baidu&wd=yt1745831259004&oq=%E5%BF%AB%E9%80%92'
 
@@ -30,6 +37,7 @@ export default {
             percent: 0,
             inputnumber,
             tokenV2,
+            tableData
         }
     },
     methods: {
@@ -50,21 +58,17 @@ export default {
             })
         },
         async getExpress() {
-            this.percent = 0;//初始化进度条的值
-            var temparr = inputnumber.value.split(",");//分割快递单号
-            var j = 0; //控制获取tokenV2的数量，查20次物流获取一次tokenV2
-            let data = []; // 用于保存所有快递信息的数组
-            await this.gettokenV2();
-            for (let i = 0; i < temparr.length; i++) {
-                j++;
-                if (j >= 20) {
-                    this.tokenV2 = await this.gettokenV2();
-                    j = 0;
+            this.percent = 0;//初始化进度条的值 
+            var expressNumbers  = inputnumber.value.split(",");//分割快递单号
+            let expressData  = []; // 用于保存所有快递信息的数组
+            for (let i = 0; i < expressNumbers.length; i++) {
+                if (i % 20 === 0) {
+                    await this.gettokenV2();
                 }
                 await axios.get(
                     'https://express.baidu.com/express/api/express?query_from_srcid=4001&' +
                     'tokenV2=' + tokenV2 + '&' +
-                    'nu=' + temparr[i]
+                    'nu=' + expressNumbers[i]
                     , {
                         headers: {
                             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
@@ -72,58 +76,52 @@ export default {
                             'Cookie': 'BAIDUID=5560EAF375A71C2A77D4D79C768210C5:FG=1;'
                         }
                     }).then(res => {
-                        res.data.data.info.expressnumber = temparr[i];// 响应结果添加快递信息
-                        data.push(res.data.data.info); 
+                        res.data.data.info.expressnumber = expressNumbers[i];// 响应结果添加快递信息
+                        expressData.push(res.data.data.info); 
                         // 计算进度条的值
-                        this.percent = Math.floor((i + 1) / temparr.length * 100);
+                        this.percent = Math.floor((i + 1) / expressNumbers.length * 100);
                     }).catch(error => {
                         console.log(error)
                     })
             }
-            //保存excel
-            console.log(data)
-            // 获取当前日期和时间
-            let now = new Date();
-            // 格式化日期和时间为字符串
-            let dateString = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate();
-            let timeString = now.getHours() + '-' + now.getMinutes() + '-' + now.getSeconds();
-            //格式化json数据为指定格式
+            //更新表格
+            this.savetableData(expressData);
+            //保存excel 
+            this.saveExcel(expressData);
+            //发送信息，成功数量通知
+            ElNotification({title: '查询物流',message: '已查询' + expressNumbers.length + '条信息',type: 'info',  duration: 0,})
+        },
+        savetableData(data){ 
+            this.tableData = [];
             const datas = data.map(({ context, ...item }) => ({
                 expressnumber: item.expressnumber,
-                // send_time: new Date(item.send_time).toLocaleString(),
                 latest_time_new: item.latest_time_new,
                 latest_progress: item.latest_progress,
-                current:item.current,
-                // ...item,
-                context: context?.map(c => `${new Date(c.time * 1000).toLocaleString()} ${c.desc}`).reverse().join('\n'),
+                current: item.current,
+                // context: context?.map(c => `${new Date(c.time * 1000).toLocaleString()} ${c.desc}`).reverse().join("\n"),
                 com: item.com
-                }));
-
-            // Create a new workbook
+            }));
+            this.tableData = datas;
+        },
+        saveExcel(data) {
+            const now = new Date();
+            const dateString = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
+            const timeString = `${now.getHours()}-${now.getMinutes()}-${now.getSeconds()}`;
+            const datas = data.map(({ context, ...item }) => ({
+                expressnumber: item.expressnumber,
+                latest_time_new: item.latest_time_new,
+                latest_progress: item.latest_progress,
+                current: item.current,
+                context: context?.map(c => `${new Date(c.time * 1000).toLocaleString()} ${c.desc}`).reverse().join("\n"),
+                com: item.com
+            }));
             const wb = XLSX.utils.book_new();
-            // Add a new worksheet to the workbook
-            const ws = XLSX.utils.json_to_sheet(datas)
-            // Set the column widths
-            ws['!cols'] = [
-            { width: 20 },
-            { width: 25 },
-            { width: 60 },
-            { width: 8 },
-            { width: 60 },
-            ];
-            XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-            // 将工作簿保存为Excel文件
-            XLSX.writeFile(wb, dateString + '-' + timeString + '智能查询' + temparr.length + '条' + '.xlsx');
-
-            //发送信息，成功数量通知
-            ElNotification({
-                title: '查询物流',
-                message: '已查询' + temparr.length + '条信息',
-                type: 'info',
-            })
+            const ws = XLSX.utils.json_to_sheet(datas);
+            ws["!cols"] = [{ width: 20 }, { width: 25 }, { width: 60 }, { width: 8 }, { width: 60 }];
+            XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
+            XLSX.writeFile(wb, dateString + '-'+timeString + '-智能查询' + data.length + '条.xlsx');
         }
-    },
-
+    }
 }
 
 </script>
@@ -135,6 +133,7 @@ export default {
 
 .el-progress--line {
     margin-top: 8px;
+    margin-bottom: 8px;
     /* width: 100; */
 }
 </style>
